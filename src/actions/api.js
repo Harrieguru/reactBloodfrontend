84abf1945c18791e6a24592fa2b4374c6de2809a
Harriet_atieno <hartie102@gmail.com>
2024-11-05 15:37:48 -0500

    production api

--- src/actions/api.js
+++ src/actions/api.js
@@ -2,12 +2,29 @@ import axios from "axios";
 import { dCandidate } from "../reducers/dCandidate";
 import { fetchAll } from "./dCandidate";
 
+const isLocalDocker = window.location.host.includes("localhost:8082");
+
 // const baseURL = "http://35.160.120.126/api"
-const baseURL = "http://localhost:30193/api/"
+const baseURL = isLocalDocker ? "http://localhost:8082/api/" : process.env.REACT_APP_API_URL || "http://localhost:30193/api/";
+// const baseURL = isLocalDocker ? "http://localhost:8082/api/" || "http://localhost:30193/api/" : process.env.REACT_APP_API_URL || "http://localhost:30193/api/";
+
+const apiURL = process.env.REACT_APP_API_URL;
 
 export default {
 
-    dCandidate(url=baseURL + 'DCandidate/'){
+    // develpment
+    // dCandidate(url=baseURL + 'DCandidate/'){
+    //     return{
+    //         fetchAll: () => axios.get(url),
+    //         fetchById: (id) => axios.get(url+id),
+    //         create : (newRecord) => axios.post(url,newRecord),
+    //         update: (id,updateRecord) => axios.put(url+id,updateRecord),
+    //         delete: (id) => axios.delete(url + id)
+    //     }
+    // }
+    // production
+
+    dCandidate(url=apiURL +'/api/' + 'DCandidate/'){
         return{
             fetchAll: () => axios.get(url),
             fetchById: (id) => axios.get(url+id),
